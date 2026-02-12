2374b61b5e636267a80d63c12bc94f3d142b1883
deppapel <maxwellomondi420@gmail.com>
2026-02-12 19:58:50 +0300

    adding db

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
     __tablename__ = "user"
-    id = db.Column(db.Integer, Primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     role = db.Column(db.String(20), default="GUEST")
