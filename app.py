09b3108b15faa6f3b059e50a04ce2bce1d6ac52e
deppapel <maxwellomondi420@gmail.com>
2026-02-19 15:30:03 +0300

    loading issue

--- app.py
+++ app.py
@@ -592,8 +592,6 @@ if __name__ == "__main__":
             db.session.add(admin)
             
         db.session.commit()
-    port = int(os.environ.get("PORT", 5000))
-    
-    app.run(host='0.0.0.0', port=port, debug=True)
+
 
 
